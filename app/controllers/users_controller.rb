161e287e88745766247058c1db16c622a70509fe
raulsposito <raulsposito@gmail.com>
2020-08-09 22:54:13 -0300

    Added authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,17 @@ class UsersController < ApplicationController
     end
 
     post "/login" do 
-        
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect '/users/#{user.id}'
+        else 
+            redirect '/login'
+        end
+
+    end
+
+    get "users/:id" do 
+        :erb 
     end
 end
